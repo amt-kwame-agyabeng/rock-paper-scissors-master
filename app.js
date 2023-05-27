fd7098afd1fa849a052e622e8a793a377dee5840
Dev-Kwame <kwame.agyabeng@amalitech.org>
2023-05-27 15:43:05 +0000

    Initial files

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const userPickChoice = (pick) => {
    //hide current page
    let choice = document.querySelector(".choice");
    choice.style.display = "none";
-  
 
+   // shows the next page when you click on your choice
+   let contest = document.querySelector(".contest");
+   contest.style.display = "flex";
 }
