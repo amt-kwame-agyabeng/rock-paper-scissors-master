35339017540cac9ae9f1e20ae9e15dd1cd5eeb1f
Dev-Kwame <kwame.agyabeng@amalitech.org>
2023-05-30 15:42:18 +0000

    Initial files

--- app.js
+++ app.js
@@ -8,4 +8,31 @@ const userPickChoice = (pick) => {
    // shows the next page when you click on your choice
    let contest = document.querySelector(".contest");
    contest.style.display = "flex";
+
+   //set the user pick
+   
+   
 }
+//Restart game
+const newGame = () =>{
+   let choice = document.querySelector(".choice");
+   choice.style.display = "";
+
+   let contest = document.querySelector(".contest");
+   contest.style.display = "none";
+
+
+}
+
+ /*
+ const housePick = () => {
+   let pick = ['rock','paper','scissors']
+   let housePick = pick[Math.floor(Math.random()*3)];
+
+   document.getElementById('.icon-scissors').src
+
+   return housePick;
+}
+*/
+
+
