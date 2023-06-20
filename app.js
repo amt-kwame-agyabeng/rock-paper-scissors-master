8722d929a4b4e78a8e1aab7986e6d8c07aa42796
Dev-Kwame <kwame.agyabeng@amalitech.org>
2023-06-20 20:05:29 +0000

    Initial files

--- app.js
+++ app.js
@@ -3,15 +3,21 @@ const modal = document.querySelector(".modal");
 const rules = document.querySelector(".rulesBtn");
 const closeButton = document.querySelector(".close-button");
 
+
+
+
+
+//Hide or show rules
 function toggleModal() {
     modal.classList.toggle("show-modal");
 }
-function windowOnClick(event) {
-    if (event.target === modal) {
-        toggleModal();
-    }
-}
+
+
 rules.addEventListener("click", toggleModal);
 closeButton.addEventListener("click", toggleModal);
 
+
 });
+
+
+
