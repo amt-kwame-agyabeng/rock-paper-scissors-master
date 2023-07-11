6742cc82613bbdb7a10ef473b43ac26758856d45
Dev-Kwame <kwame.agyabeng@amalitech.org>
2023-07-11 15:46:40 +0000

    Initial files

--- app.js
+++ app.js
@@ -1,23 +1,94 @@
-document.addEventListener("DOMContentLoaded", () => {
-const modal = document.querySelector(".modal");
-const rules = document.querySelector(".rulesBtn");
-const closeButton = document.querySelector(".close-button");
+const choices =[
+    {
+        name: "paper",
+        beats: "rock"
+    },
+    {
+        name: "scissors",
+        beats: "paper"
+    },
+    {
+        name: "rock",
+        beats: "scissors"
+    },
+]
+const choiceButtons = document.querySelectorAll('.pick-btn');
+const choiceDiv = document.querySelector('.choice')
+const gameDiv = document.querySelector('.game')
+const gamePick = document.querySelectorAll('.game_pick')
+
+ //game logic
+ choiceButtons.forEach(button =>{
+    button.addEventListener('click',() => {
+        const choiceName = button.dataset.choice;
+        const choice = choices.find(choice => choice.name === choiceName)
+        choose(choice)
+
+    })
+ }) 
+ 
+ function choose(choice){
+    const computerChoice = computerChoose()
+    displayResults([choice,computerChoice])
+
+ }
+ function computerChoose(){
+    const rand = Math.floor(Math.random()* choices.length)
+    return choices[rand]
+ }
+
+ function displayResults(results){
+    gamePick.forEach((gameDiv, idx) => {
+        setTimeout(() => {
+            gameDiv.innerHTML = `
+            <div class = "choice ${results[idx].name}">
+            <img src = images/icon-${results[idx].name}.svg" alt = ${results[idx].name}"/>
+            </div>
+            `
+
+
+        }, idx*1000)
+
+    })
+
+ }
+
+  
+
+    
+   
+    
+
+    
+    
+    
+    
+    
+    const modal = document.querySelector(".modal");
+    const rules = document.querySelector(".rulesBtn");
+    const closeButton = document.querySelector(".close-button");
+    
+    //Hide or show rules modal
+    function toggleModal(){
+        modal.classList.toggle("show-modal");
+    }
+    
+    rules.addEventListener("click", toggleModal);
+    closeButton.addEventListener("click", toggleModal);
+  
 
 
 
 
 
-//Hide or show rules
-function toggleModal() {
-    modal.classList.toggle("show-modal");
-}
 
 
-rules.addEventListener("click", toggleModal);
-closeButton.addEventListener("click", toggleModal);
 
 
-});
+
+
+
+
 
 
 
