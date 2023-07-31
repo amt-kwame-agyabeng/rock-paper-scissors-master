3327cfb3e2c7af53a224faa8a2ba369d516c8109
Dev-Kwame <kwame.agyabeng@amalitech.org>
2023-07-31 21:45:41 +0000

    Initial files

--- app.js
+++ app.js
@@ -1,42 +1,39 @@
-const choices =[
-  {
-      name: "paper",
-      beats: "rock"
-  },
-  {
-      name: "scissors",
-      beats: "paper"
-  },
-  {
-      name: "rock",
-      beats: "scissors"
-  },
-]
-
-
-const choiceButtons = document.querySelectorAll('.pick-btn')
 const choiceDiv = document.querySelector('.player-choice')
+const choices = document.querySelectorAll('.choice')
 const game = document.querySelector('.game')
-const gamePick = document.querySelectorAll('.game_pick')
+const gameResults = document.querySelectorAll('.game_pick')
 const gameWinner = document.querySelector('.game_winner')
-const gameWinnerText = document.querySelector('.game_text')
-const scoreNum = document.querySelector('#score-num')
+const gameWinnerText = document.querySelector('#game_text')
+const scores = document.querySelector('#score-num')
 const playAgain = document.querySelector('.play-again')
+
 
 let score = 0;
 
 
-
-
-//game logic
-choiceButtons.forEach(button =>{
-  button.addEventListener('click',() => {
-      const choiceName = button.dataset.choice;
-      const choice = choices.find(choice => choice.name === choiceName);
-      choose(choice)
-
-  });
-});
+const picks = [
+  {
+    name: "paper",
+    beats: "rock"
+  },
+  {
+    name: "scissors",
+    beats: "paper"
+  },
+  {
+    name: "rock",
+    beats: "scissors"
+  },
+]
+ 
+//game play
+choices.forEach(choices =>{
+  choices.addEventListener('click', () => {
+    const choiceName = choices.dataset.choice;
+    const choice = picks.find(choice => choice.name === choiceName);
+    choose(choice)
+  })
+})
 
 function choose(choice){
   const computerChoice = computerChoose();
@@ -45,72 +42,66 @@ function choose(choice){
 }
 
 function computerChoose(){
-  const rand = Math.floor(Math.random()* choices.length);
-  return choices[rand]
+  const rand = Math.floor(Math.random()*picks.length)
+  return picks[rand];
 }
 
 function displayResults(results){
-  gamePick.forEach((gameDiv, idx) => {
-      setTimeout(() => {
-          gameDiv.innerHTML = `
-          <div class = "choice ${results[idx].name}">
-          <img src = "images/icon-${results[idx].name}.svg" >
-          </div>
-          `;
+  gameResults.forEach((game, idx)=>{
+  setTimeout(() => {
+    game.innerHTML =`<div class = "choice ${results[idx].name}">
+    <img src = "images/icon-${results[idx].name}.svg" >
+    </div>
+    `;
+    
+  },idx*2000);
+  })
 
-
-      }, idx*3000);
-  
-  });
-  
-  choiceDiv.classList.toggle('hidden');
-  game.classList.toggle('hidden');
+  choiceDiv.classList.toggle('hidden')
+  game.classList.toggle('hidden')
 }
 
 function displayWinner(results){
   setTimeout(() => {
-     const userWins = isWinner(results) 
-     const computerWins = isWinner(results.reverse())
-     if (userWins) {
-      gameWinnerText.innerText ="YOU WIN"
-      gamePick[0].classList.toggle('winner')
+    const userWins = isWinner(results)
+    const computerWins = isWinner(results.reverse())
+    if (userWins){
+      gameWinnerText.innerText = "YOU WIN";
       updateScore(1)
-     }else if(computerWins){
-      gameWinnerText.innerText ="YOU LOSE"
-      gamePick[1].classList.toggle('winner')
-      playAgain.innertext
-      playAgain.style.color = '#DB2E4D';
-      
-     updateScore(-1) 
-     }else{
-      gameWinnerText.innerText = "DRAW"
-     }
-     gameWinner.classList.toggle('hidden');
-     game.classList.toogle('show-winner');
+      gameResults[0].classList.toggle('winner');
 
+    }else if(computerWins){
+      gameWinnerText.innerText = "YOU LOSE";
+      updateScore(-1)
+      gameResults[1].classList.toggle('winner')
+      playAgain.style.color ="#DB2E4D";
+    }else{
+      gameWinnerText.innerText = "DRAW";
+    }
+
+    gameWinner.classList.toggle('hidden');
+    game.classList.toggle('show-winnner')
   }, 5000);
 
   
 }
 
-function isWinner(results) {
-  return results[0].beats === results[1].name;
-}
-
 function updateScore(point){
   score += point
-  scoreNum.innerText = score;
+  scores.innerText = score;
 
 }
 
+function isWinner(results) {
+ return results[0].beats === results[1].name;
+}
 
-
-//Restart Game
+//Restart game
 playAgain.addEventListener('click', () => {
   choiceDiv.classList.toggle('hidden');
   game.classList.toggle('hidden');
 
-  gamePick.forEach((resultDiv) => {
+  gameResults.forEach((resultDiv) => {
     resultDiv.innerHTML = "";
     resultDiv.classList.remove('winner');
   });
@@ -121,11 +112,9 @@ playAgain.addEventListener('click', () => {
 });
 
 
-  
 
 
-  
-  
+
   const modal = document.querySelector(".modal");
   const rules = document.querySelector(".rulesBtn");
   const closeButton = document.querySelector(".close-button");
