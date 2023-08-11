0569ad53ea20c5736e68422409b74d157f2d1b29
Dev-Kwame <kwame.agyabeng@amalitech.org>
2023-08-11 15:51:13 +0000

    Initial files

--- app.js
+++ app.js
@@ -7,9 +7,13 @@ const gameWinner = document.querySelector('.game_winner')
 const gameWinnerText = document.querySelector('#game_text')
 const scores = document.querySelector('#score-num')
 const playAgain = document.querySelector('.play-again')
-
+const bonus = document.querySelector('.bonus-btn')
+const bonusGame = document.querySelector('.logo-bonus')
+const logo = document.querySelector('.logo')
+const gameBonus = document.querySelector('.bonus-game')
 
 let score = 0;
+let bonusMode = false;
 
 
 const picks = [
@@ -25,32 +29,70 @@ const picks = [
     name: "rock",
     beats: "scissors"
   },
-]
+];
+
+const bonusPicks =[
+  {
+    name: "scissors",
+    beats: ["paper", "lizard"],
+  },
+  {
+    name: "paper",
+    beats: ["rock", "spock"],
+  },
+  {
+    name: "rock",
+    beats: ["lizard","scissors"],
+  },
+  {
+    name: "spock",
+    beats: ["scissors", "lizard"],
+  },
+  {
+    name: "lizard",
+    beats: ["spock", "paper"],
+  },
+  
+];
  
 //game play
 choices.forEach(choices =>{
   choices.addEventListener('click', () => {
     const choiceName = choices.dataset.choice;
-    const choice = picks.find(choice => choice.name === choiceName);
-    choose(choice)
-  })
-})
+    if(bonusMode) {
+      const choice = bonusPicks.find(choice => choice.name === choiceName);
+      chooseBonus(choice);
+    }else {
+     const choice = picks.find(choice => choice.name === choiceName)
+     choose(choice) 
+    }
+   
+  });
+});
 
 function choose(choice){
   const computerChoice = computerChoose();
   displayResults([choice,computerChoice]);
   displayWinner([choice,computerChoice]);
 }
+function chooseBonus(choice){
+  const computerChoice = computerChooseBonus();
+  displayResultsBonus([choice,computerChoice]);
+  displayWinnerBonus([choice,computerChoice]);
+}
 
 function computerChoose(){
   const rand = Math.floor(Math.random()*picks.length)
   return picks[rand];
 }
-
+function computerChooseBonus(){
+  const rand = Math.floor(Math.random()*bonusPicks.length)
+  return bonusPicks[rand]
+}
 function displayResults(results){
   gameResults.forEach((game, idx)=>{
   setTimeout(() => {
-    game.innerHTML =`<div class = "choice ${results[idx].name}">
+    game.innerHTML =`<div class = "choice ${results[idx].name} ">
     <img src = "images/icon-${results[idx].name}.svg" >
     </div>
     `;
@@ -60,6 +102,20 @@ function displayResults(results){
 
   choiceDiv.classList.toggle('hidden')
   game.classList.toggle('hidden')
+}
+function displayResultsBonus(results){
+  gameResults.forEach((game, idx)=>{
+  setTimeout(() => {
+    game.innerHTML = `<div class = "choice ${results[idx].name}">
+    <img src = "images/icon-${results[idx].name}.svg" >
+    </div>
+    `;
+    
+  },idx*3000);
+  })
+
+  gameBonus.classList.toggle('hidden');
+  game.classList.toggle('hidden');
 }
 
 function displayWinner(results){
@@ -86,21 +142,76 @@ function displayWinner(results){
 
   
 }
+function displayWinnerBonus(results){
+  setTimeout(() => {
+    const userWins = isWinnerBonus(results)
+
+    if (userWins === true){
+      gameWinnerText.innerText = "YOU WIN";
+      updateScore(1)
+      gameResults[0].classList.toggle('winner');
+    }else if(userWins === false){
+      gameWinnerText.innerText = "YOU LOSE";
+      updateScore(-1)
+      gameResults[1].classList.toggle('winner')
+      playAgain.style.color ="#DB2E4D";
+    }else{
+      gameWinnerText.innerText = "DRAW";
+    }
+
+    gameWinner.classList.toggle('hidden');
+    game.classList.toggle('show-winnner')
+  }, 5000);
+
+
+}
+function resetScores(){
+  score = 0
+  scores.innerText = score;
+} 
 
 function updateScore(point){
   score += point
   scores.innerText = score;
 
+//store the updated score in local storage
+  localStorage.setItem("score", score)
+
 }
+//Check if score exists in local storage
+const savedScore = localStorage.getItem("score")
+
+if(savedScore !== null){
+  score = parseInt(savedScore)
+  scores.innerText = score;
+
+}
+
+
 
 function isWinner(results) {
  return results[0].beats === results[1].name;
 }
 
+function isWinnerBonus(results) {
+  const userChoice = results[0];
+  const computerChoice = results[1];
+
+  if(userChoice.name === computerChoice.name) {
+    return "draw";
+  }
+  else{
+    return userChoice.beats[0] === computerChoice.name || userChoice.beats[1] === computerChoice.name
+  };
+ }
+ 
+
+
 //Restart game
 playAgain.addEventListener('click', () => {
-  choiceDiv.classList.toggle('hidden');
-  game.classList.toggle('hidden');
+  if(!bonusMode){
+    choiceDiv.classList.toggle('hidden');
+    game.classList.toggle('hidden');
 
   gameResults.forEach((game) => {
     game.innerHTML = "";
@@ -109,11 +220,60 @@ playAgain.addEventListener('click', () => {
 
   gameWinnerText.innerText = "";
   gameWinner.classList.toggle('hidden');
+  game.classList.toggle('show-winner');    
+  }else if(bonusMode){
+    gameBonus.classList.toggle('hidden')
+    game.classList.toggle('hidden');
+  
+  gameResults.forEach((game) => {
+    game.innerHTML = "";
+    game.classList.remove('winner');
+  });
+
+  
+  gameWinnerText.innerText = "";
+  gameWinner.classList.toggle('hidden');
   game.classList.toggle('show-winner');
+
+ }
+  
 });
 
 
+bonus.addEventListener("click",() =>{
+  resetScores ();
 
+  bonusMode = !bonusMode;
+
+
+
+  bonusGame.classList.toggle('hidden');
+  logo.classList.toggle('hidden');
+
+  
+  gameResults.forEach((game) => {
+    game.innerHTML = "";
+    game.classList.remove('winner');
+  });
+
+  if(bonusMode){
+    bonus.innerText = "NORMAL";
+    
+    choiceDiv.classList.add('hidden');
+    gameBonus.classList.remove('hidden');
+    game.classList.remove('show-winner');
+    game.classList.add('hidden');
+    gameWinner.classList.add('hidden');
+  }else{
+    bonus.innerText = "BONUS"
+    choiceDiv.classList.remove('hidden');
+    gameBonus.classList.add('hidden');
+    game.classList.remove('show-winner');
+    game.classList.add('hidden');
+    gameWinner.classList.add('hidden');
+
+  }
+});
 
 
   const modal = document.querySelector(".modal");
